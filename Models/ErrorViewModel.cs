2cbe2ace5079d1b71aeb09286dcf08870134defc
Steven <steven@ispan.com.tw>
2025-07-23 08:55:01 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Msit66Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
