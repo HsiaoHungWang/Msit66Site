d7d14479bef322624ae562b305efbd106bdea3fc
HsiaoHungWang <wang@ispan.com.tw>
2025-07-23 11:08:07 +0800

    _layout使用說明

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace Msit66Site.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
